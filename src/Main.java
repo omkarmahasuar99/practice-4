3d0b01f9c71e0e2bb2f6122965e4c2c18bc80569
omkarmahasuar99 <126793930+omkarmahasuar99@users.noreply.github.com>
2023-03-22 13:20:04 +0530

    odd and positive

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int x=9;
+        int rem=x%2;
+        if (rem==1)
+            if(x>0)
+                System.out.println("true");
+            else System.out.println("false");
+        else System.out.println("false");
+    }
+}
